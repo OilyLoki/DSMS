43a8deed9edafce5a4e3cf9ef3af11e16bf2bee4
Adam Hemer <adamhemer32@gmail.com>
2020-11-16 00:34:52 +1030

    Updated Emails

--- dsms-dat.sql
+++ dsms-dat.sql
@@ -1,22 +1,23 @@
 
-INSERT INTO Staff VALUES ("53424", "WILLIAM", "MOORE", "MR", "61 784 315 851", "@gmail.com");
-INSERT INTO Staff VALUES ("94105", "ELIZABETH", "TAYLOR", "MRS", "61 886 182 178", "@gmail.com");
-INSERT INTO Staff VALUES ("30780", "DAVID", "ANDERSON", "MR", "61 801 897 983", "@gmail.com");
-INSERT INTO Staff VALUES ("82420", "BARBARA", "THOMAS", "MRS", "61 792 671 136", "@gmail.com");
-INSERT INTO Staff VALUES ("95246", "RICHARD", "JACKSON", "MR", "61 381 937 940", "@gmail.com");
-INSERT INTO Staff VALUES ("84070", "SUSAN", "WHITE", "MRS", "61 270 549 443", "@gmail.com");
-INSERT INTO Staff VALUES ("98458", "JOSEPH", "HARRIS", "MR", "61 718 924 844", "@gmail.com");
-INSERT INTO Staff VALUES ("35765", "JESSICA", "MARTIN", "MRS", "61 862 428 686", "@gmail.com");
-INSERT INTO Staff VALUES ("45037", "THOMAS", "THOMPSON", "MR", "61 597 532 659", "@gmail.com");
-INSERT INTO Staff VALUES ("53418", "SARAH", "GARCIA", "MRS", "61 456 624 910", "@gmail.com");
-INSERT INTO Staff VALUES ("68504", "CHARLES", "MARTINEZ", "MR", "61 759 075 082", "@gmail.com");
-INSERT INTO Staff VALUES ("88003", "KAREN", "ROBINSON", "MRS", "61 072 976 122", "@gmail.com");
-INSERT INTO Staff VALUES ("97382", "CHRISTOPHER", "CLARK", "MR", "61 176 635 233", "@gmail.com");
-INSERT INTO Staff VALUES ("92553", "NANCY", "RODRIGUEZ", "MRS", "61 387 871 401", "@gmail.com");
-INSERT INTO Staff VALUES ("61980", "DANIEL", "LEWIS", "MR", "61 463 892 860", "@gmail.com");
-INSERT INTO Staff VALUES ("24925", "LISA", "LEE", "MRS", "61 014 676 897", "@gmail.com");
-INSERT INTO Staff VALUES ("77283", "MATTHEW", "WALKER", "MR", "61 553 904 758", "@gmail.com");
-INSERT INTO Staff VALUES ("22613", "MARGARET", "HALL", "MRS", "61 380 006 372", "@gmail.com");
+INSERT INTO Staff VALUES ("53424", "WILLIAM", "MOORE", "MR", "CARER", "61 784 315 851", "williammoore@gmail.com");
+INSERT INTO Staff VALUES ("94105", "ELIZABETH", "TAYLOR", "MRS", "CARER", "61 886 182 178", "elizabethtaylor@gmail.com");
+INSERT INTO Staff VALUES ("30780", "DAVID", "ANDERSON", "MR", "CARER", "61 801 897 983", "davidanderson@gmail.com");
+INSERT INTO Staff VALUES ("82420", "BARBARA", "THOMAS", "MRS", "CARER", "61 792 671 136", "barbarathomas@gmail.com");
+INSERT INTO Staff VALUES ("95246", "RICHARD", "JACKSON", "MR", "CARER", "61 381 937 940", "richardjackson@gmail.com");
+INSERT INTO Staff VALUES ("84070", "SUSAN", "WHITE", "MRS", "CARER", "61 270 549 443", "susanwhite@gmail.com");
+INSERT INTO Staff VALUES ("98458", "JOSEPH", "HARRIS", "MR", "CARER", "61 718 924 844", "josephharris@gmail.com");
+INSERT INTO Staff VALUES ("35765", "JESSICA", "MARTIN", "MRS", "CARER", "61 862 428 686", "jessicamartin@gmail.com");
+INSERT INTO Staff VALUES ("45037", "THOMAS", "THOMPSON", "MR", "CARER", "61 597 532 659", "thomasthompson@gmail.com");
+INSERT INTO Staff VALUES ("53418", "SARAH", "GARCIA", "MRS", "CARER", "61 456 624 910", "sarahgarcia@gmail.com");
+INSERT INTO Staff VALUES ("68504", "CHARLES", "MARTINEZ", "MR", "CARER", "61 759 075 082", "charlesmartinez@gmail.com");
+INSERT INTO Staff VALUES ("88003", "KAREN", "ROBINSON", "MRS", "CARER", "61 072 976 122", "karenrobinson@gmail.com");
+INSERT INTO Staff VALUES ("97382", "CHRISTOPHER", "CLARK", "MR", "CARER", "61 176 635 233", "christopherclark@gmail.com");
+INSERT INTO Staff VALUES ("92553", "NANCY", "RODRIGUEZ", "MRS", "CARER", "61 387 871 401", "nancyrodriguez@gmail.com");
+INSERT INTO Staff VALUES ("61980", "DANIEL", "LEWIS", "MR", "CARER", "61 463 892 860", "daniellewis@gmail.com");
+INSERT INTO Staff VALUES ("24925", "LISA", "LEE", "MRS", "CARER", "61 014 676 897", "lisalee@gmail.com");
+INSERT INTO Staff VALUES ("77283", "MATTHEW", "WALKER", "MR", "CARER", "61 553 904 758", "matthewwalker@gmail.com");
+INSERT INTO Staff VALUES ("22613", "MARGARET", "HALL", "MRS", "CARER", "61 380 006 372", "margarethall@gmail.com");
+
 
 INSERT INTO Participant
 
