e5f0bd5c9143ffa1121c1a3b922947ce01f311aa
Adam Hemer <adamhemer32@gmail.com>
2020-11-16 00:12:09 +1030

    Initial Commit

---
+++ dsms-bld.sql
@@ -0,0 +1,102 @@
+SET foreign_key_checks = 0;
+
+DROP TABLE Staff;
+DROP TABLE Appointment;
+DROP TABLE Participant;
+DROP TABLE Relations;
+DROP TABLE Guardian;
+DROP TABLE Behaviour;
+DROP TABLE Category;
+DROP TABLE Subcat;
+
+SET foreign_key_checks = 1;
+
+CREATE TABLE Staff
+(
+    staffID      CHAR(5),
+    fName        VARCHAR(20),
+    lName        VARCHAR(20),
+    title        VARCHAR(3),
+    position     VARCHAR(10),
+    phoneNumber  VARCHAR(14),
+    emailAddress VARCHAR(30),
+    PRIMARY KEY (staffID)
+);
+
+CREATE TABLE Appointment
+(
+    dateTime      DATETIME,
+    staffID       CHAR(5),
+    participantID CHAR(5),
+    PRIMARY KEY (dateTime, staffID, participantID),
+    FOREIGN KEY (staffID) REFERENCES Staff (staffID),
+    FOREIGN KEY (participantID) REFERENCES Participant (participantID)
+);
+
+
+CREATE TABLE Participant
+(
+    participantID    CHAR(5),
+    ndisID           CHAR(10),
+    dateOfBirth      DATE,
+    fName            VARCHAR(20),
+    lName            VARCHAR(20),
+    contact          VARCHAR(30),
+    preferredSupport CHAR(5),
+    PRIMARY KEY (participantID),
+    FOREIGN KEY (preferredSupport) REFERENCES Staff (staffID)
+);
+
+CREATE TABLE Relations
+(
+    relationship  VARCHAR(10),
+    participantID CHAR(5),
+    guardianID    CHAR(5),
+    PRIMARY KEY (participantID, guardianID),
+    FOREIGN KEY (participantID) REFERENCES Participant (participantID),
+    FOREIGN KEY (guardianID) REFERENCES Guardian (guardianID)
+);
+
+CREATE TABLE Guardian
+(
+    guardianID   CHAR(5),
+    fName        VARCHAR(20),
+    lName        VARCHAR(20),
+    phoneNumber  VARCHAR(14),
+    emailAddress VARCHAR(30),
+    PRIMARY KEY (guardianID)
+);
+
+CREATE TABLE Behaviour
+(
+    behaviourID   CHAR(9),
+    time          TIME,
+    duration      SMALLINT,
+    severity      TINYINT,
+    location      VARCHAR(20),
+    participantID CHAR(5),
+    categoryID    CHAR(1),
+    subID         CHAR(1),
+    PRIMARY KEY (behaviourID),
+    FOREIGN KEY (participantID) REFERENCES Participant (participantID),
+    FOREIGN KEY (categoryID) REFERENCES Category (categoryID),
+    FOREIGN KEY (subID) REFERENCES SubCat (subID)
+);
+
+CREATE TABLE Category
+(
+    categoryID CHAR(1),
+    name       VARCHAR(20),
+    descr      VARCHAR(20),
+    PRIMARY KEY (categoryID)
+);
+
+CREATE TABLE Subcat
+(
+    subID      CHAR(1),
+    name       VARCHAR(20),
+    descr      VARCHAR(20),
+    categoryID CHAR(1),
+    PRIMARY KEY (subID),
+    FOREIGN KEY (categoryID) REFERENCES Category (categoryID)
+);
---
+++ dsms-dat.sql
@@ -0,0 +1,42 @@
+
+INSERT INTO Staff VALUES ("53424", "WILLIAM", "MOORE", "MR", "61 784 315 851", "@gmail.com");
+INSERT INTO Staff VALUES ("94105", "ELIZABETH", "TAYLOR", "MRS", "61 886 182 178", "@gmail.com");
+INSERT INTO Staff VALUES ("30780", "DAVID", "ANDERSON", "MR", "61 801 897 983", "@gmail.com");
+INSERT INTO Staff VALUES ("82420", "BARBARA", "THOMAS", "MRS", "61 792 671 136", "@gmail.com");
+INSERT INTO Staff VALUES ("95246", "RICHARD", "JACKSON", "MR", "61 381 937 940", "@gmail.com");
+INSERT INTO Staff VALUES ("84070", "SUSAN", "WHITE", "MRS", "61 270 549 443", "@gmail.com");
+INSERT INTO Staff VALUES ("98458", "JOSEPH", "HARRIS", "MR", "61 718 924 844", "@gmail.com");
+INSERT INTO Staff VALUES ("35765", "JESSICA", "MARTIN", "MRS", "61 862 428 686", "@gmail.com");
+INSERT INTO Staff VALUES ("45037", "THOMAS", "THOMPSON", "MR", "61 597 532 659", "@gmail.com");
+INSERT INTO Staff VALUES ("53418", "SARAH", "GARCIA", "MRS", "61 456 624 910", "@gmail.com");
+INSERT INTO Staff VALUES ("68504", "CHARLES", "MARTINEZ", "MR", "61 759 075 082", "@gmail.com");
+INSERT INTO Staff VALUES ("88003", "KAREN", "ROBINSON", "MRS", "61 072 976 122", "@gmail.com");
+INSERT INTO Staff VALUES ("97382", "CHRISTOPHER", "CLARK", "MR", "61 176 635 233", "@gmail.com");
+INSERT INTO Staff VALUES ("92553", "NANCY", "RODRIGUEZ", "MRS", "61 387 871 401", "@gmail.com");
+INSERT INTO Staff VALUES ("61980", "DANIEL", "LEWIS", "MR", "61 463 892 860", "@gmail.com");
+INSERT INTO Staff VALUES ("24925", "LISA", "LEE", "MRS", "61 014 676 897", "@gmail.com");
+INSERT INTO Staff VALUES ("77283", "MATTHEW", "WALKER", "MR", "61 553 904 758", "@gmail.com");
+INSERT INTO Staff VALUES ("22613", "MARGARET", "HALL", "MRS", "61 380 006 372", "@gmail.com");
+
+INSERT INTO Participant
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
